d0b829a4e05318cb9099205c96867c25dfb06443
Waszker <Waszka23@gmail.com>
2015-03-04 00:29:07 +0100

    Added flag interpreter that fill all required info provided by user.

--- ComputationalCluster/src/ComputationalServer/ComputationalServerMainActivity.java
+++ ComputationalCluster/src/ComputationalServer/ComputationalServerMainActivity.java
@@ -1,21 +1,31 @@
 package ComputationalServer;
 
+import java.net.UnknownHostException;
 import java.util.Map;
+
+import GenericCommonClasses.GenericFlagInterpreter;
 
 public class ComputationalServerMainActivity
 {
 	public static void main(String[] args)
 	{
-		Map<String, Object> flagsMap = GenericFlagInterpreter
-				.interpretFlags(args);
-
-		ComputationalServerWindow window = new ComputationalServerWindow(
-				flagsMap.get("isBackup") == null);
-
-		if (flagsMap.get("isGui") != null)
+		Map<String, Object> flagsMap;
+		try
 		{
-			window.setVisible(true);
-		}
+			flagsMap = GenericFlagInterpreter.interpretFlags(args);
 
+			ComputationalServerWindow window = new ComputationalServerWindow(
+					flagsMap.get("isBackup") == null);
+
+			if (flagsMap.get("isGui") != null)
+			{
+				window.setVisible(true);
+			}
+
+		}
+		catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
--- ComputationalCluster/src/GenericCommonClasses/GenericFlagInterpreter.java
+++ ComputationalCluster/src/GenericCommonClasses/GenericFlagInterpreter.java
@@ -1,10 +1,9 @@
 package GenericCommonClasses;
 
-import java.util.HashMap;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Map;
-
-import javax.swing.JButton;
-import javax.swing.JTextField;
+import java.util.TreeMap;
 
 public abstract class GenericFlagInterpreter
 {
@@ -12,22 +11,55 @@ public abstract class GenericFlagInterpreter
 	/******************/
 	/* VARIABLES */
 	/******************/
-	static Map<String, Object> FlagMap;
 
 	/******************/
 	/* FUNCTIONS */
 	/******************/
-	
-	public Map interpretFlags(String[] args)
+
+	/**
+	 * <p>
+	 * Function interpret flags that were given to program at start. It returns
+	 * map containing information provided by user during startup.
+	 * </p>
+	 * 
+	 * @param args array of arguments provided during startup
+	 * @return map containing information in proper way
+	 * @throws UnknownHostException
+	 */
+	public static Map<String, Object> interpretFlags(String[] args)
+			throws UnknownHostException
 	{
-		FlagMap=new HashMap<String, Object>();
-		for(String s : args)
+		Map<String, Object> flagMap = new TreeMap<>();
+
+		// TODO: Add error handling!
+		for (int i = 0; i < args.length; i += 2)
 		{
-			if(args[0].contentEquals("-address"))
+			switch (args[i])
 			{
-			
+				case "-port":
+					flagMap.put("port", Integer.parseInt(args[i + 1]));
+					break;
+
+				case "-address":
+					flagMap.put("address", InetAddress.getByName(args[i + 1]));
+					break;
+
+				case "-t":
+					flagMap.put("timeout", Integer.parseInt(args[i + 1]));
+					break;
+
+				case "-backup":
+					flagMap.put("isBackup", true);
+					i--;
+					break;
+
+				case "-gui":
+					flagMap.put("isGui", true);
+					i--;
+					break;
 			}
 		}
-		return FlagMap;
+
+		return flagMap;
 	}
 }
