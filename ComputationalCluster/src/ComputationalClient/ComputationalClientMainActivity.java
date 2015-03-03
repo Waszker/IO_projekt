e66a060194ecffafc337e1d563dcf4e67c32ffb5
anyanyany <a.zawadzka@op.pl>
2015-03-03 22:15:51 +0100

    A little change in ComputationalClientMainActivity - added try-catch block

--- ComputationalCluster/src/ComputationalClient/ComputationalClientMainActivity.java
+++ ComputationalCluster/src/ComputationalClient/ComputationalClientMainActivity.java
@@ -1,15 +1,32 @@
 package ComputationalClient;
 
+import java.net.UnknownHostException;
+import java.util.Map;
+
+import ComputationalServer.ComputationalServerWindow;
+import GenericCommonClasses.GenericFlagInterpreter;
 
 public class ComputationalClientMainActivity
 {
 
 	public static void main(String[] args)
 	{
-		// TODO: determine if user wants GUI
-		ComputationalClientWindow window = new ComputationalClientWindow();
-		window.setVisible(true);
+		Map<String, Object> flagsMap;
+		try
+		{
+			flagsMap = GenericFlagInterpreter.interpretFlags(args);
 
+			ComputationalClientWindow window = new ComputationalClientWindow();
+
+			if (flagsMap.get("isGui") != null)
+			{
+				window.setVisible(true);
+			}
+
+		} catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
