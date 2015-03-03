35718bfecfc31159191211e5fca98c745b848c3c
Filip <fturkot@gmail.com>
2015-03-03 23:05:42 +0100

    Added flags handling to Task Manager.

--- ComputationalCluster/src/TaskManager/TaskManagerMainActivity.java
+++ ComputationalCluster/src/TaskManager/TaskManagerMainActivity.java
@@ -1,5 +1,9 @@
 package TaskManager;
 
+import java.net.UnknownHostException;
+import java.util.Map;
+import GenericCommonClasses.GenericFlagInterpreter;
+
 
 /**
  * @author Filip Turkot
@@ -13,7 +17,20 @@ public class TaskManagerMainActivity
 {
 	public static void main(String[] args)
 	{
-		TaskManagerWindow mainWindow = new TaskManagerWindow();
-		mainWindow.setVisible(true);
+		Map<String, Object> flagsMap = null;
+		try
+		{
+			flagsMap = GenericFlagInterpreter.interpretFlags(args);
+		}
+		catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
+		
+		if (flagsMap.get("isGui") != null)
+		{
+			TaskManagerWindow mainWindow = new TaskManagerWindow();
+			mainWindow.setVisible(true);
+		}
 	}
 }
