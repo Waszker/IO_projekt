085cf796ff3de598b70a42a1b8047d1ea470eee5
waszker <waszka23@gmail.com>
2015-03-04 12:02:27 +0100

    Fixed Start Server button not disabling after clicking it. Server now awaits connection from many different clients.

--- ComputationalCluster/src/ComputationalServer/ComputationalServerCore.java
+++ ComputationalCluster/src/ComputationalServer/ComputationalServerCore.java
@@ -34,59 +34,65 @@ public class ComputationalServerCore
 	 * 
 	 * @param port
 	 *            on which messages are expected
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public void startListening(int port) throws IOException
 	{
 		this.port = port;
 
 		ServerSocket socket = new ServerSocket(port);
-		connectionSocket = socket.accept();
-		System.out.println("Connection estabilished!");
-		connectionSocket.close();
-		socket.close(); // TODO: This should probably not be here!		
+		while (true)
+		{
+			connectionSocket = socket.accept();
+			System.out.println("Connection estabilished!");
+		}
+		//connectionSocket.close();
+		//socket.close(); // TODO: This should probably not be here!
 	}
-	
-//	Thing that not work at the time:
-//	private void listenForMessages() {
-//		new Thread(new Runnable() {
-//			
-//			@Override
-//			public void run() {
-//				try {					
-//					while(true) 
-//					{
-//						ObjectInputStream inFromServer = new ObjectInputStream(socket.getInputStream());
-//						Message message;
-//						if((message = (Message)inFromServer.readObject())!= null)
-//						{
-//							if(message.getMessageType() == Message.Type.Text)
-//							{
-//								System.out.println(message.getMessageContent());
-//							}
-//							else
-//							{
-//								Files.write(Paths.get(message.getMessageContent()), message.getFileContent(), StandardOpenOption.CREATE);
-//							}
-//						}
-//						Thread.sleep(100);
-//					}
-//					
-//				} catch (IOException | InterruptedException | ClassNotFoundException e) {
-//					e.printStackTrace();
-//				}
-//				
-//			}
-//		}).start();
-//	}
-//	
-//	private void sendMessage(Message message) {
-//		try {
-//			ObjectOutputStream outToClient = new ObjectOutputStream(socket.getOutputStream());
-//			outToClient.writeObject(message);
-//		} catch (IOException e1) {
-//			e1.printStackTrace();
-//		}
-//	}
+
+	// Thing that not work at the time:
+	// private void listenForMessages() {
+	// new Thread(new Runnable() {
+	//
+	// @Override
+	// public void run() {
+	// try {
+	// while(true)
+	// {
+	// ObjectInputStream inFromServer = new
+	// ObjectInputStream(socket.getInputStream());
+	// Message message;
+	// if((message = (Message)inFromServer.readObject())!= null)
+	// {
+	// if(message.getMessageType() == Message.Type.Text)
+	// {
+	// System.out.println(message.getMessageContent());
+	// }
+	// else
+	// {
+	// Files.write(Paths.get(message.getMessageContent()),
+	// message.getFileContent(), StandardOpenOption.CREATE);
+	// }
+	// }
+	// Thread.sleep(100);
+	// }
+	//
+	// } catch (IOException | InterruptedException | ClassNotFoundException e) {
+	// e.printStackTrace();
+	// }
+	//
+	// }
+	// }).start();
+	// }
+	//
+	// private void sendMessage(Message message) {
+	// try {
+	// ObjectOutputStream outToClient = new
+	// ObjectOutputStream(socket.getOutputStream());
+	// outToClient.writeObject(message);
+	// } catch (IOException e1) {
+	// e1.printStackTrace();
+	// }
+	// }
 
 }
