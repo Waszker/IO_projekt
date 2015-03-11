067cc05542ae80d1ef10f0882c71ffc752714656
Filip <fturkot@gmail.com>
2015-03-11 20:41:20 +0100

    Further merge

--- ComputationalCluster/src/TaskManager/TaskManagerWindowActionListener.java
+++ ComputationalCluster/src/TaskManager/TaskManagerWindowActionListener.java
@@ -1,9 +1,16 @@
 package TaskManager;
 
 import GenericCommonClasses.GenericWindowActionListener;
+import GenericCommonClasses.GenericWindowGui;
 
 public class TaskManagerWindowActionListener extends
 		GenericWindowActionListener
 {
+
+	public TaskManagerWindowActionListener(GenericWindowGui window)
+	{
+		super(window);
+		// TODO Auto-generated constructor stub
+	}
 
 }
