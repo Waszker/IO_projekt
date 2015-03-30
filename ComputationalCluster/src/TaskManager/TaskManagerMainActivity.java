22c7ba319f0adbfd7e9b67b338c3dcbbbffb2149
Filip Turkot <fturkot@gmail.com>
2015-03-30 15:07:32 +0200

    Added usage

--- ComputationalCluster/src/TaskManager/TaskManagerMainActivity.java
+++ ComputationalCluster/src/TaskManager/TaskManagerMainActivity.java
@@ -14,6 +14,12 @@ import GenericCommonClasses.GenericFlagInterpreter;
  */
 public class TaskManagerMainActivity
 {
+	static void usage()
+	{
+		System.err.println("usage: $java -jar TM.jar -port <port_number> -addres <ip_address>");
+		System.exit(-1);
+	}
+	
 	public static void main(String[] args)
 	{
 		try
@@ -26,6 +32,9 @@ public class TaskManagerMainActivity
 					.get(GenericFlagInterpreter.FLAG_ADDRESS);
 			Integer serverPort = (Integer) flagsMap
 					.get(GenericFlagInterpreter.FLAG_PORT);
+			
+			if ( serverIp == null )usage();
+			if ( serverPort == null )usage();
 
 			TaskManager taskManager = new TaskManager(serverIp, serverPort,
 					isGuiEnabled);
@@ -41,9 +50,9 @@ public class TaskManagerMainActivity
 				taskManager.connectToServer();
 			}
 		}
-		catch (NumberFormatException | UnknownHostException e)
+		catch (NumberFormatException|UnknownHostException e)
 		{
-			e.printStackTrace();
+			usage();
 		}
 	}
 }
