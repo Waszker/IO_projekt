c82812650e36ee181f564454c466bef5d69eb336
Waszker <Waszka23@gmail.com>
2015-04-19 19:03:19 +0200

    Initial DVRP commit.

---
+++ ComputationalCluster/src/Problems/DVRP/Definition/DVRPProblemDefinition.java
@@ -0,0 +1,41 @@
+package Problems.DVRP.Definition;
+
+/**
+ * <p>
+ * Class representing DVRP problem instance. It consists of data required to
+ * solve it and offers method for quick to-byte-(de)serialization.
+ * </p>
+ * 
+ * @author Piotr Waszkiewicz
+ * @version 1.0
+ * 
+ */
+public class DVRPProblemDefinition
+{
+	/******************/
+	/* VARIABLES */
+	/******************/
+
+	/******************/
+	/* FUNCTIONS */
+	/******************/
+	/**
+	 * <p>
+	 * Serializes class to byte array for easy over-the-net sending.
+	 * <p>
+	 * 
+	 * @return serialized object
+	 */
+	public byte[] serialize()
+	{
+		// TODO: Implement serialization
+		return null;
+	}
+
+	public static DVRPProblemDefinition deserialize(byte[] data)
+	{
+		// TODO: Implement deserialization or specific constructor?
+		return null;
+	}
+
+}
