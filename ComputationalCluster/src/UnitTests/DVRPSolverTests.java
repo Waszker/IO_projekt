97161e1654661b84d4690c6263ab7e6a1b6fbd28
FilipT <turkotf@student.mini.pw.edu.pl>
2015-05-30 17:47:42 +0200

    Optimized dividing graph algorithm

--- ComputationalCluster/src/UnitTests/DVRPSolverTests.java
+++ ComputationalCluster/src/UnitTests/DVRPSolverTests.java
@@ -5,16 +5,17 @@ import java.math.BigInteger;
 
 import org.junit.Test;
 
+import Problems.DVRPSolver;
 import Problems.DVRPProblem.Client;
 import Problems.DVRPProblem.Depot;
 import Problems.DVRPProblem.Graph;
+import Problems.DVRPProblem.IGraphNode;
 
 public class DVRPSolverTests
 {
 	@Test
 	public void graphPartitionigTest() throws IOException
-	{
-		/*
+	{/*
 		String inputMessage = "2 1.0 1.0 2" + "\n" +
 							  "0.0 0.0 0.0 1000000.0" + "\n" + 
 							  "0.0 1.0 0.0 10.0 1.0"+ "\n" +
@@ -26,39 +27,48 @@ public class DVRPSolverTests
 		byte[] problemData = inputMessage.getBytes();
 		DVRPSolver solver = new DVRPSolver(problemData); 
 		
-		byte[][] res = solver.DivideProblem(10);
+		byte[][] res = solver.DivideProblem(1);
 		
-		//for ( int i=0; i<res.length; i++ )
-		//	res[i] = solver.Solve(res[i], 0);
+		for ( byte[] b : res )
+			System.out.println( new String(b) );
 		
-		//byte[] finalResult = solver.MergeSolution(res);
-		//System.out.println(new String(finalResult));*/
+		for ( int i=0; i<res.length; i++ )
+			res[i] = solver.Solve(res[i], 0);
 		
-		Depot d = new Depot(0, 0, 5, 20);
+		byte[] finalResult = solver.MergeSolution(res);
+		System.out.println(new String(finalResult));*/
+		
+		final int NOV = 3;
+		Depot d = new Depot(0, 0, 0, 10);
 		Client[] c = new Client[]
 				{
-					new Client(1, 1, 0, 0, 5),
-					new Client(2, 1, 0, 0, 5),
-					new Client(3, 1, 0, 0, 5),
-					new Client(4, 1, 0, 0, 5),
-					new Client(5, 1, 0, 0, 5),
-					new Client(6, 1, 0, 0, 5),
-					new Client(7, 1, 0, 0, 5),
-					new Client(8, 1, 0, 0, 5),
-					new Client(9, 1, 0, 0, 5),
-					new Client(1, 2, 0, 0, 5),
-					new Client(1, 3, 0, 0, 5),
-					new Client(1, 4, 0, 0, 5)
+					new Client(0, 1, 0, 0, 0),
+					new Client(0, 2, 0, 0, 0),
+					new Client(1, 0, 0, 0, 0),
+					new Client(0, 2, 0, 0, 0),
+					new Client(1, 0, 0, 0, 0),
+					new Client(0, 2, 0, 0, 0),
+					new Client(1, 0, 0, 0, 0),
+					new Client(0, 2, 0, 0, 0),
+					new Client(0, 2, 0, 0, 0),
+					new Client(1, 0, 0, 0, 0),
+					new Client(0, 2, 0, 0, 0),
+					new Client(1, 0, 0, 0, 0),
+					new Client(0, 2, 0, 0, 0),
+					new Client(0, 2, 0, 0, 0),
+					new Client(1, 0, 0, 0, 0)
 				};
 		
-		Graph given = new Graph(d, c);
+		Graph g = new Graph(d, c);
+		g.initialize(BigInteger.ZERO, null, NOV);
 		
-		int i = 0;
-		Graph[] gset;
-		do
-		{
-			gset = given.divideGraph(3, BigInteger.valueOf(i++));
-		}while ( gset != null );
+		System.out.println( "Clients: " + c.length + ", Vehicles: " + NOV );
+		
+		int i=0;
+		while ( g.nextPartitioning()!=null )
+			i++;
+		System.out.println(i);
+		System.out.println("(old: "+BigInteger.valueOf(NOV).pow(c.length) + ")");
 	}
 
 }
